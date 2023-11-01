d334bad3f258bbb9cdefff9672155d71531009b9
Nevmetal <85611291+Nevmetal@users.noreply.github.com>
2023-11-01 20:46:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,51 @@
+// Create a web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+const server = http.createServer(handleRequest);
+server.listen(port, () => {
+    console.log('Server listening on: http://localhost:%s', port);
+});
+function handleRequest(req, res) {
+    let filePath = '.' + req.url;
+    if (filePath == './')
+        filePath = './index.html';
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                res.end();
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}
