760aa312f1d8442669036d7fcd56cf7e1b758491
Nevmetal <85611291+Nevmetal@users.noreply.github.com>
2023-11-01 21:14:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,65 +1,31 @@
-// Create a web server
-const express = require("express");
+// Create web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const comments = require("./data/comments");
-const products = require("./data/products");
-const port = process.env.PORT || 4001;
-
-// Use static server to serve the Express Yourself Website
-app.use(express.static(path.join(__dirname, "public")));
-
-// Parse request body if content-type is JSON
 app.use(bodyParser.json());
 
-app.get("/comments", (req, res) => {
-  res.send(comments);
-});
+// Create event handler for CommentCreated event
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  console.log('Event received:', type);
 
-app.get("/comments/:commentId", (req, res) => {
-  const commentId = Number(req.params.commentId);
-  const getComment = comments.find(comment => comment.id === commentId);
-  if (getComment) {
-    res.send(getComment);
-  } else {
-    res.status(404).send("Comment not found");
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const status = content.includes('orange') ? 'rejected' : 'approved';
+
+    // Emit CommentModerated event
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentModerated',
+      data: { id, content, postId, status }
+    });
   }
+
+  res.send({});
 });
 
-app.post("/comments", (req, res) => {
-  const newComment = {
-    id: req.params.id,
-    body: req.params.body,
-    postId: req.params.postId
-  };
-  comments.push(newComment);
-  res.send(newComment);
+app.listen(4003, () => {
+  console.log('Listening on 4003');
 });
-
-app.put("/comments/:commentId", (req, res) => {
-  const commentId = Number(req.params.commentId);
-  const updateComment = comments.find(comment => comment.id === commentId);
-  if (updateComment) {
-    updateComment.body = req.params.body;
-    res.send(updateComment);
-  } else {
-    res.status(404).send("Comment not found");
-  }
-});
-
-app.delete("/comments/:commentId", (req, res) => {
-  const commentId = Number(req.params.commentId);
-  const deleteComment = comments.find(comment => comment.id === commentId);
-  if (deleteComment) {
-    deleteComment.body = req.params.body;
-    res.send(deleteComment);
-  } else {
-    res.status(404).send("Comment not found");
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Web server is listening on port ${port}!`);
-});
-//comentario para finalizar commit
