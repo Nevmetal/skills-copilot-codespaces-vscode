559540b67dfb65038450d262dce7712501eb0037
Nevmetal <85611291+Nevmetal@users.noreply.github.com>
2023-11-01 20:57:51 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,15 @@
 // Create a web server
-// Run with: node comments.js
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-// Tell the web server to serve files
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-var comments = [];
-
-app.get('/comments.json', function(req, res) {
-  res.json(comments);
-});
-
-app.post('/comments', function(req, res) {
-  var comment = req.body;
-  console.log(comment);
-  comments.push(comment);
-  res.json(comment);
-});
-
-app.listen(3000, function() {
-  console.log('Server listening on port 3000');
-});
+// The web server should respond to requests to /comments with a web page that lists the comments people have left.
+// It should have a form on this page that lets people leave new comments: it has fields for author, email, and comment itself.
+// The web server should respond to POST requests to /comments by storing the comment object in a global variable.
+// It should then redirect back to /comments.
+// Test it all by opening several tabs to http://localhost:8000/comments and leaving comments in each of them.
+// Notice how they all show up when you go back to http://localhost:8000/comments.
+// To test the POST functionality, you can use curl (from the command line) or Poster (a Firefox plug-in).
+// This is a rather silly way to store comments, since comments will be lost when the server shuts down, but it'll do for now.
+// When you're done, run npm install express@3 jade body-parser in the directory where you put your code to install the dependencies.
+// You can then run the program with the command node server.js.
+// Note that this is the old version of Express, which is slightly different from the current version.
+// If you want to use the current version, you'll have to look up the documentation.
+// If you get stuck, the code for this exercise is in the express directory in the source code for this book.
+// It is also available online at
